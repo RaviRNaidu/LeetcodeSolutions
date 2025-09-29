fa170b8de828a6e840a4e22bae8b5f9d09ca85e9
RaviRNaidu <164465669+RaviRNaidu@users.noreply.github.com>
2025-09-29 21:06:17 +0530

    Time: 11 ms (17.70%) | Memory: 13.7 MB (7.76%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -3,7 +3,7 @@
  * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
+ *     ListNode(int x) : val(x), next(NULL) {}  
  * };
  */
 class Solution {
@@ -11,13 +11,11 @@ public:
     ListNode *detectCycle(ListNode *head) {
         map<ListNode* ,int> mpp;
         ListNode* temp = head;
-        int n = 0;
         while(temp != NULL){
             if(mpp.find(temp) != mpp.end()){
                 return temp;
             }
-            mpp.insert({temp,n});
-            n++;
+            mpp[temp]++;
             temp = temp->next;
         }
         return NULL;
