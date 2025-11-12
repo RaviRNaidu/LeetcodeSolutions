15e6c9f4cd397de77b0174d7fab19791818ddd11
RaviRNaidu <164465669+RaviRNaidu@users.noreply.github.com>
2025-11-13 00:21:29 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (94.08%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,14 +1,5 @@
 class Solution {
 public:
-    bool isalnum(char c){
-        if((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')){
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
-
     bool isPalindrome(string s) {
         int n = s.length();
         int start = 0;
