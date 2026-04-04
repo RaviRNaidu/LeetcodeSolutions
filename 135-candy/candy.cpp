0ef85e11d294f77f6d8b646ec798915a7f0f1664
Ravi R Naidu <164465669+RaviRNaidu@users.noreply.github.com>
2026-04-04 12:26:22 +0530

    Time: 3 ms (25.01%) | Memory: 23 MB (52.77%) - LeetSync

---
+++ 135-candy/candy.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        vector<int> left(n);
+        left[0] = 1;
+        for(int i=1;i<n;i++){
+            if(ratings[i] > ratings[i-1]){
+                left[i] = left[i-1] + 1;
+            }
+            else{
+                left[i] = 1;
+            }
+        }
+
+        int ans = left[n-1];
+        int right = 1;
+        int curr = 1;
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i] > ratings[i+1]){
+                curr = right + 1;
+            }
+            else{
+                curr = 1;
+            }
+            right = curr;
+            ans += max(left[i],curr);
+        }
+
+        return ans;
+    }
+};
