1201f2aa6dbbb3fe93f035de745c5dab86b7caa0
Ravi R Naidu <164465669+RaviRNaidu@users.noreply.github.com>
2026-05-22 17:17:39 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (17.68%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -3,18 +3,27 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
+        vector<vector<int>> vis(n, vector<int>(m,0));
 
         queue<pair<int, pair<int, int>>> q;
+        
+        int frestOranges = 0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j] == 2){
                     q.push({0 , {i, j}});
+                    vis[i][j] = 1;
+                }
+                else if(grid[i][j] == 1){
+                    frestOranges++;
                 }
             }
         }
 
         int drow[4] = {-1, 0, 1, 0};
         int dcol[4] = {0, 1, 0 ,-1};
+
+        int rottenOranges = 0;
 
         int timer = 0;
         while(!q.empty()){
@@ -27,20 +36,15 @@ public:
                 int nrow = row + drow[i];
                 int ncol = col + dcol[i];
 
-                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && grid[nrow][ncol] == 1){
-                    grid[nrow][ncol] = 2;
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && grid[nrow][ncol] == 1 && vis[nrow][ncol] == 0){
+                    vis[nrow][ncol] = 1;
+                    rottenOranges++;
                     q.push({time + 1,{nrow, ncol}});
                 }
             }
         }
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j] == 1){
-                    return -1;
-                }
-            }
-        }
+        if(rottenOranges != frestOranges) return -1;
 
         return timer;
     }
