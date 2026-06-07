c93aa32caf331d0a3de35cba5e4a7ca8a5a8cd5d
Ravi R Naidu <164465669+RaviRNaidu@users.noreply.github.com>
2026-06-07 18:17:44 +0530

    Time: 8 ms (22.34%) | Memory: 18.5 MB (49.59%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,42 +1,39 @@
 class Solution {
-    bool dfs(int node, vector<bool> &vis, vector<int> adj[], stack<int> &st, vector<int> &path){
-        vis[node] = true;
-        path[node] = 1;
-        for(auto it : adj[node]){
-            if(vis[it] && path[it]){
-                return false;
-            }
-            else if(!vis[it]){
-                if(dfs(it, vis, adj, st, path) == false) return false;
-            }
-        }
-        path[node] = 0;
-        st.push(node);
-        return true;
-    }
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         vector<int> adj[numCourses];
+        vector<int> inDeg(numCourses,0);
         for(auto it : prerequisites){
             int u = it[1];
             int v = it[0];
             adj[u].push_back(v);
+            inDeg[v] += 1;
         }
 
         vector<bool> vis(numCourses, false);
-        vector<int> path(numCourses, 0);
-        stack<int> st;
+        queue<int> q;
         vector<int> ans;
         for(int i=0;i<numCourses;i++){
-            if(!vis[i]){
-                if(dfs(i, vis, adj, st, path) == false) return ans;
+            if(!vis[i] && inDeg[i] == 0){
+                q.push(i);
+                vis[i] = true;
             }
         }
-        while(!st.empty()){
-            ans.push_back(st.top());
-            st.pop();
-        }
 
-        return ans;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+
+            for(auto it : adj[node]){
+                inDeg[it]--;
+                if(inDeg[it] == 0){
+                    vis[it] = true;
+                    q.push(it);
+                }
+            }
+        }
+        if(ans.size() != numCourses) return {};
+        else return ans;
     }
 };
