a728d38f3e5bb3abc74284f7509e0292e016aa78
Ravi R Naidu <164465669+RaviRNaidu@users.noreply.github.com>
2026-06-29 11:10:45 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (33.74%) - LeetSync

--- 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.cpp
+++ 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.cpp
@@ -5,13 +5,8 @@ public:
         int ans = 0;
         for(auto it : patterns){
             string temp = it;
-            int size = temp.size();
-            for(int i=0;i<m;i++){
-                string check = word.substr(i, size);
-                if(check == temp){
-                    ans++;
-                    break;
-                }
+            if(word.contains(temp)){
+                ans++;
             }
         }
         return ans;
