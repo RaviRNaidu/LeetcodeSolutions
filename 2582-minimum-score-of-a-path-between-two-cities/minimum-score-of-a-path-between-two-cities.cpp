111dd54131dc88fa694f2ffc6090a2bcc4314dd6
Ravi R Naidu <164465669+RaviRNaidu@users.noreply.github.com>
2026-07-05 02:15:29 +0530

    Time: 103 ms (31.85%) | Memory: 130.5 MB (82.71%) - LeetSync

--- 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.cpp
+++ 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.cpp
@@ -1,28 +1,62 @@
-class Solution {
-    void dfs(int node, vector<pair<int, int>> adj[], vector<int> &vis, int &ans){
-        vis[node] = 1;
-        for(auto it : adj[node]){
-            int newNode = it.first;
-            int dis = it.second;
-            ans = min(ans, dis);
-            if(!vis[newNode]){
-                dfs(newNode, adj, vis, ans);
-            }
+class DisjointSet{
+    public:
+    vector<int> parent, size;
+
+    public:
+    DisjointSet(int n){
+        parent.resize(n);
+        size.resize(n, 1);
+        for(int i=0;i<n;i++){
+            parent[i] = i;
         }
     }
+
+    int findUPar(int node){
+        if(parent[node] == node){
+            return node;
+        }
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionBySize(int u, int v){
+        int up_u = findUPar(u);
+        int up_v = findUPar(v);
+
+        if(up_u == up_v){
+            return;
+        }
+        else if(size[up_u] < size[up_v]){
+            size[up_v] += size[up_u];
+            parent[up_u] = up_v;
+        }
+        else{
+            size[up_u] += size[up_v];
+            parent[up_v] = up_u;
+        }
+    }
+};
+class Solution {
 public:
     int minScore(int n, vector<vector<int>>& roads) {
-        vector<int> vis(n+1, 0);
-        vector<pair<int, int>> adj[n+1];
+        DisjointSet ds(n+1);
         for(auto it : roads){
             int u = it[0];
             int v = it[1];
-            int d = it[2];
-            adj[u].push_back({v, d});
-            adj[v].push_back({u, d});
+            int d = it[2]; 
+            ds.unionBySize(u, v);
         }
+
+        int p = ds.findUPar(1);
         int ans = INT_MAX;
-        dfs(1, adj, vis, ans);
+        for(auto it : roads){
+            int u = it[0];
+            int v = it[1];
+            int d = it[2]; 
+            
+            if(ds.findUPar(u) == p){
+                ans = min(ans, d);
+            }
+        }
         return ans;
     }
 };
