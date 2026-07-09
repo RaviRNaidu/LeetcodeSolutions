5f4877918101a6b322c827c6c63c22c00527d835
Ravi R Naidu <164465669+RaviRNaidu@users.noreply.github.com>
2026-07-09 21:23:53 +0530

    Time: 1 ms (9.18%) | Memory: 10.7 MB (23.23%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,19 +1,14 @@
 class Solution {
-    int recursion(int i, vector<int> &nums, vector<int> &dp){
-        if(i == 0) return dp[i] = 0;
-        if(i == 1) return dp[i] = nums[i-1];
-
-        if(dp[i] != -1) return dp[i];
-
-        int take = nums[i-1] + recursion(i-2, nums, dp);
-        int notTake = recursion(i-1, nums, dp);
-
-        return dp[i] = max(take, notTake);
-    }
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n+1, -1);
-        return recursion(n, nums, dp);
+        vector<int> dp(n+1, 0);
+        dp[1] = nums[0];
+        for(int i=2;i<=n;i++){
+            int take = nums[i-1] + dp[i-2];
+            int notTake = dp[i-1];
+            dp[i] = max(take, notTake);
+        }
+        return dp[n];
     }
 };
