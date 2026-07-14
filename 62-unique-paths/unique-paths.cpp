68a33a649cf4cf987890c7e70aec998a0200d35d
Ravi R Naidu <164465669+RaviRNaidu@users.noreply.github.com>
2026-07-14 18:00:41 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (77.12%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,17 +1,31 @@
 class Solution {
-    int recursion(int i, int j, vector<vector<int>> &dp){
-        if(i < 0 || j < 0) return 0;
-        if(i == 0 && j == 0) return dp[i][j] = 1;
+    // int recursion(int i, int j, vector<vector<int>> &dp){
+    //     if(i == 0 || j == 0) return 0;
+    //     if(i == 1 && j == 1) return dp[i][j] = 1;
 
-        if(dp[i][j] != -1) return dp[i][j];
+    //     if(dp[i][j] != -1) return dp[i][j];
 
-        int up = recursion(i-1, j, dp);
-        int left = recursion(i, j-1, dp);
-        return dp[i][j] = (up + left);
-    }
+    //     int up = recursion(i-1, j, dp);
+    //     int left = recursion(i, j-1, dp);
+    //     return dp[i][j] = (up + left);
+    // }
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int> (n,-1));
-        return recursion(m-1, n-1, dp);
+        vector<int> prev(n+1, 0);
+        for(int i=1;i<=m;i++){
+            vector<int> curr(n+1, 0);
+            for(int j=1;j<=n;j++){
+                if(i == 1 && j == 1){
+                    curr[j] = 1;
+                }
+                else{
+                    int up = prev[j];
+                    int left = curr[j-1];
+                    curr[j] = up + left;
+                }
+            }
+            prev = curr;
+        }
+        return prev[n];
     }
 };
