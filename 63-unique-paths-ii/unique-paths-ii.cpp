ad2106800f69b09805c757eee9d275f0cdb662cb
Ravi R Naidu <164465669+RaviRNaidu@users.noreply.github.com>
2026-07-15 19:43:01 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (85.64%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,21 +1,27 @@
 class Solution {
-    int recursion(int i, int j, vector<vector<int>> &obstacleGrid, vector<vector<int>> &dp){
-        if(i == 0 || j == 0) return dp[i][j] = 0;
-        if(i == 1 && j == 1 && !obstacleGrid[i-1][j-1]) return dp[i][j] = 1;
-        if(obstacleGrid[i-1][j-1] == 1) return dp[i][j] = 0;
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        int left = recursion(i, j-1, obstacleGrid, dp);
-        int up = recursion(i-1, j, obstacleGrid, dp);
-
-        return dp[i][j] = left + up;
-    }
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int n = obstacleGrid.size();
         int m = obstacleGrid[0].size();
-        vector<vector<int>> dp(n+1, vector<int> (m+1, -1));
-        return recursion(n, m, obstacleGrid, dp);
+        vector<int> prev(m+1, 0);
+        for(int i=1;i<=n;i++){
+            vector<int> curr(m+1, 0);
+            for(int j=1;j<=m;j++){
+                if(i == 1 && j == 1 && !obstacleGrid[i-1][j-1]){
+                    curr[j] = 1;
+                }
+                else if(obstacleGrid[i-1][j-1]){
+                    curr[j] = 0;
+                }
+                else{
+                    int left = curr[j-1];
+                    int up = prev[j];
+
+                    curr[j] = left + up;
+                } 
+            }
+            prev = curr;
+        }
+        return prev[m];
     }
 };
