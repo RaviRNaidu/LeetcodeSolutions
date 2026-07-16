3d5bfe2b7412fd40a38acbc0d69af7d31fb26888
Ravi R Naidu <164465669+RaviRNaidu@users.noreply.github.com>
2026-07-16 16:42:49 +0530

    Time: 0 ms (100.00%) | Memory: 16 MB (73.90%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,20 +1,24 @@
 class Solution {
-    int recursion(int i, int j, vector<vector<int>> &grid, vector<vector<int>> &dp){
-        if(i == 0 || j == 0) return dp[i][j] = 1e7;
-        if(i == 1 && j == 1) return dp[i][j] = grid[i-1][j-1];
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        int up = grid[i-1][j-1] + recursion(i-1, j, grid, dp);
-        int left = grid[i-1][j-1] + recursion(i, j-1, grid, dp);
-
-        return dp[i][j] = min(up, left);
-    }
 public:
     int minPathSum(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>> dp(n+1, vector<int> (m+1,-1));
-        return recursion(n, m, grid, dp);
+        vector<int> prev(m+1, 1e9);
+        for(int i=1;i<=n;i++){
+            vector<int> curr(m+1, 1e9);
+            for(int j=1;j<=m;j++){
+                if(i == 1 && j == 1){
+                    curr[j] = grid[i-1][j-1];
+                }
+                else{
+                    int up = grid[i-1][j-1] + prev[j];
+                    int left = grid[i-1][j-1] + curr[j-1];
+                    curr[j] = min(up, left);
+                }
+            }
+            prev = curr;
+        }
+
+        return prev[m];
     }
 };
