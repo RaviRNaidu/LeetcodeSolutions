293504cd7930eaf345ca9d6ef79e61099d48d0fd
Ravi R Naidu <164465669+RaviRNaidu@users.noreply.github.com>
2026-07-17 12:03:02 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (93.03%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -8,14 +8,12 @@ public:
         }
 
         for(int i=n-2;i>=0;i--){
-            vector<int> prev(n);
             for(int j=0;j<triangle[i].size();j++){
                 int down = triangle[i][j] + front[j];
                 int rightDown = triangle[i][j] + front[j+1];
 
-                prev[j] = min(down, rightDown);
+                front[j] = min(down, rightDown);
             }
-            front = prev;
         }
         return front[0];
     }
