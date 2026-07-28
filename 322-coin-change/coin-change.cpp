31666730cf39ed23cb6217e8bafed9ddb5cd78a1
Ravi R Naidu <164465669+RaviRNaidu@users.noreply.github.com>
2026-07-28 21:33:36 +0530

    Time: 26 ms (65.27%) | Memory: 18 MB (74.35%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,23 +1,23 @@
 class Solution {
-    int recursion(int i, int amount, vector<int> &coins, vector<vector<int>> &dp){
-        if(amount == 0) return dp[i][amount] = 0;
-        if(i == 0) return dp[i][amount] = 1e9;
-
-        if(dp[i][amount] != -1) return dp[i][amount];
-        
-        int notTake = recursion(i-1, amount, coins,dp);
-        int take = 1e9;
-        if(coins[i - 1] <= amount){
-            take = 1 + recursion(i, amount - coins[i - 1], coins, dp);
-        }
-
-        return dp[i][amount] = min(take, notTake);
-    }
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n+1, vector<int> (amount+1, -1));
-        int ans = recursion(n, amount, coins, dp);
+
+        vector<int> prev(amount+1, 1e9);
+        prev[0] = 0;
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=amount;j++){
+                int notTake = prev[j];
+                int take = 1e9;
+                if(coins[i - 1] <= j){
+                    take = 1 + prev[j - coins[i - 1]];
+                }
+                prev[j] = min(take, notTake);
+            }
+        }
+
+        int ans = prev[amount];
         if(ans == 1e9) return -1;
         return ans;
     }
