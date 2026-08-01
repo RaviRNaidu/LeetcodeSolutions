6ab970c47583c40df0413b1f45875859eae06b9b
Ravi R Naidu <164465669+RaviRNaidu@users.noreply.github.com>
2026-08-01 14:59:28 +0530

    Time: 2 ms (43.58%) | Memory: 9.9 MB (54.28%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,19 +1,22 @@
 class Solution {
-    int recursion(int i, int j, vector<int> &nums, vector<vector<int>> &dp){
-        if (i == j) return dp[i][j] = nums[i];
-
-        if(dp[i][j] != -1) return dp[i][j];
-    
-        int pickLeft  = nums[i] - recursion(i + 1, j, nums, dp);
-        int pickRight = nums[j] - recursion(i, j - 1, nums, dp);
-        
-        return dp[i][j] = max(pickLeft, pickRight);
-    }
 public:
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n+1, vector<int> (n+1, -1));
-        int sum = recursion(0, n-1, nums, dp);
+        vector<int> dp(n+1, 0);
+        for(int i=n-1;i>=0;i--){
+            for(int j=i;j<n;j++){
+                if(i == j){
+                    dp[i] = nums[i];
+                }
+                else{
+                    int pickLeft  = nums[i] - dp[j];
+                    int pickRight = nums[j] - dp[j-1];
+                    
+                    dp[j] = max(pickLeft, pickRight);
+                }
+            }
+        }
+        int sum = dp[n-1];
         if(sum >= 0) return true;
         return false;
     }
