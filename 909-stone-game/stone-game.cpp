bcb0d201852e625f6a6e9c584991ddf99815e5c8
Ravi R Naidu <164465669+RaviRNaidu@users.noreply.github.com>
2026-08-02 10:32:55 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (46.05%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,25 +1,6 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        int n = piles.size();
-        vector<int> dp(n, 0);
-
-        for(int i=n-1;i>=0;i--){
-            for(int j=i;j<n;j++){
-                if(i == j){
-                    dp[j] = piles[i];
-                }
-                else{
-                    int left = piles[i] - dp[j];
-                    int right = piles[j] - dp[j-1];
-
-                    dp[j] = max(left, right);
-                }
-            }
-        }
-
-        int ans = dp[n-1];
-        if(ans > 0) return true;
-        return false;
+        return true;
     }
 };
