b23efff31a71561b78137f6eccaee2908073ec0a
Ravi R Naidu <164465669+RaviRNaidu@users.noreply.github.com>
2026-08-08 00:05:59 +0530

    Time: 2 ms (88.13%) | Memory: 15.2 MB (58.79%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,19 +1,4 @@
 class Solution {
-    int recursion(int i, vector<int> &nums, int target, vector<vector<int>> &dp){
-        if(i == 0){
-            if(target == 0 && nums[i] == 0) return dp[i][target] = 2;
-            if(target == nums[i] || target == 0) return dp[i][target] = 1;
-            return dp[i][target] = 0;
-        }
-
-        int notTake = recursion(i-1, nums, target, dp);
-        int take = 0;
-        if(nums[i] <= target){
-            take = recursion(i-1, nums, target - nums[i], dp);
-        }
-
-        return dp[i][target] = notTake + take;
-    }
 public:
     int findTargetSumWays(vector<int>& nums, int d) {
         int n = nums.size();
@@ -23,8 +8,24 @@ public:
         }
         if(sum - d < 0 || ((sum - d) % 2)) return 0;
         int target = (sum - d) / 2;
-        vector<vector<int>> dp(n, vector<int> (target+1, -1));
-        int ans = recursion(n-1, nums, target, dp);
+
+        vector<int> prev(target+1, 0);
+        if(nums[0] == 0) prev[0] = 2;
+        else prev[0] = 1;
+        if(nums[0] != 0 && nums[0] <= target) prev[nums[0]] = 1;
+        for(int i=1;i<n;i++){
+            vector<int> curr(target+1, 0);
+            for(int j=0;j<=target;j++){
+                int notTake = prev[j];
+                int take = 0;
+                if(nums[i] <= j){
+                    take = prev[j - nums[i]];
+                }
+                curr[j] = notTake + take;
+            }
+            prev = curr;
+        }
+        int ans = prev[target];
         return ans;
     }
 };
