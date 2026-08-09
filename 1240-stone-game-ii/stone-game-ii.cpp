973e6f5cf4eff597366b8726fc40620aad7a67d9
Ravi R Naidu <164465669+RaviRNaidu@users.noreply.github.com>
2026-08-09 13:26:55 +0530

    Time: 7 ms (77.21%) | Memory: 13.7 MB (43.80%) - LeetSync

---
+++ 1240-stone-game-ii/stone-game-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    int recursion(int idx, vector<int> &piles, int m, vector<int> &suffix, int n, vector<vector<int>>& dp){
+        if(idx == n) return dp[idx][m] = 0;
+
+        if(dp[idx][m] != -1) return dp[idx][m];
+
+        int allow = 2 * m;
+        int ans = INT_MIN;
+        for(int i=1;i<=allow;i++){
+            if(idx + i <= n){
+                ans = max(ans, (suffix[idx] - suffix[idx + i]) - recursion(idx + i, piles, max(i, m), suffix, n, dp));
+            }
+        }
+        if(ans == INT_MIN) return dp[idx][m] = 0;
+        return dp[idx][m] = ans;
+    }
+public:
+    int stoneGameII(vector<int>& piles) {
+        int n = piles.size();
+        vector<int> suffix(n+1, 0);
+        for(int i=n-1;i>=0;i--){
+            suffix[i] = piles[i] + suffix[i+1];
+        }
+        int m = 1;
+        vector<vector<int>> dp(n+1, vector<int> (n+1, -1));
+        int ans = recursion(0, piles, m, suffix, n, dp);
+        ans = (suffix[0] + ans) / 2;
+        return ans;
+    }
+};
