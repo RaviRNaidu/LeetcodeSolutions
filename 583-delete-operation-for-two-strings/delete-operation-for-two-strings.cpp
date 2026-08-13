3816b7bb7e526d00432ca9adf94aef80150d4311
Ravi R Naidu <164465669+RaviRNaidu@users.noreply.github.com>
2026-08-14 00:32:25 +0530

    Time: 19 ms (13.37%) | Memory: 16.8 MB (55.52%) - LeetSync

---
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    int recursion(int i, int j, string &word1, string &word2, vector<vector<int>> &dp){
+        if(i == 0 || j == 0) return dp[i][j] = 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        if(word1[i-1] == word2[j-1]){
+            return dp[i][j] = 1 + recursion(i-1, j-1, word1, word2, dp);
+        }
+
+        int first = recursion(i-1, j, word1, word2, dp);
+        int second = recursion(i, j-1, word1, word2, dp);
+
+        return dp[i][j] = max(first, second);
+    }
+public:
+    int minDistance(string word1, string word2) {
+        int n = word1.size();
+        int m = word2.size();
+        vector<vector<int>> dp(n+1, vector<int> (m+1, -1));
+        int ans = recursion(n, m, word1, word2, dp);
+        return (n + m) - 2 * ans;
+    }
+};
