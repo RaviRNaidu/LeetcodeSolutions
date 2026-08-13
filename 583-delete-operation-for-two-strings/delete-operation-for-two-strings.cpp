5516eb52c454277d075e471fd71d49700f7fb502
Ravi R Naidu <164465669+RaviRNaidu@users.noreply.github.com>
2026-08-14 00:36:28 +0530

    Time: 12 ms (49.27%) | Memory: 15.6 MB (80.68%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -1,24 +1,25 @@
 class Solution {
-    int recursion(int i, int j, string &word1, string &word2, vector<vector<int>> &dp){
-        if(i == 0 || j == 0) return dp[i][j] = 0;
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        if(word1[i-1] == word2[j-1]){
-            return dp[i][j] = 1 + recursion(i-1, j-1, word1, word2, dp);
-        }
-
-        int first = recursion(i-1, j, word1, word2, dp);
-        int second = recursion(i, j-1, word1, word2, dp);
-
-        return dp[i][j] = max(first, second);
-    }
 public:
     int minDistance(string word1, string word2) {
         int n = word1.size();
         int m = word2.size();
-        vector<vector<int>> dp(n+1, vector<int> (m+1, -1));
-        int ans = recursion(n, m, word1, word2, dp);
+        vector<int> prev(m+1, 0);
+        for(int i=1;i<=n;i++){
+            vector<int> curr(m+1, 0);
+            for(int j=1;j<=m;j++){
+                if(word1[i-1] == word2[j-1]){
+                    curr[j] = 1 + prev[j-1];
+                }
+                else{
+                    int first = prev[j];
+                    int second = curr[j-1];
+
+                    curr[j] = max(first, second);
+                }
+            }
+            prev = curr;
+        }
+        int ans = prev[m];
         return (n + m) - 2 * ans;
     }
 };
