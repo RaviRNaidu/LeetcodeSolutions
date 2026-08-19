8eb06d69b9be084b4f82d26731cb4753a65e9d4d
Ravi R Naidu <164465669+RaviRNaidu@users.noreply.github.com>
2026-08-19 16:44:20 +0530

    Time: 11 ms (87.81%) | Memory: 9 MB (91.55%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -6,18 +6,15 @@ public:
         vector<long long> prev(m+1, 0);
         prev[0] = 1;
         for(int i=1;i<=n;i++){
-            vector<long long> curr(m+1, 0);
-            curr[0] = 1;
-            for(int j=1;j<=m;j++){
+            for(int j=m;j>=1;j--){
                 int take = 0;
                 if(s[i-1] == t[j-1]){
                     take = prev[j-1];
                 }
 
                 long long notTake = prev[j];
-                curr[j] = take + notTake;
+                prev[j] = take + notTake;
             }
-            prev = curr;
         }
         return (int)prev[m];
     }
