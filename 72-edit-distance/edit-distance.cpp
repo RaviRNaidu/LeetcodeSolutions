91bfd27f4ba2e169c88986e7e324a1d0e5aca82b
Ravi R Naidu <164465669+RaviRNaidu@users.noreply.github.com>
2026-08-20 14:48:23 +0530

    Time: 1 ms (95.21%) | Memory: 12.5 MB (83.64%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,26 +1,30 @@
 class Solution {
-    int recursion(int i, int j, string &word1, string &word2, vector<vector<int>>& dp){
-        if(i == 0 && j == 0) return dp[i][j] = 0;
-        if(i == 0 && j) return dp[i][j] = j;
-        if(j == 0 && i) return dp[i][j] = i;
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        if(word1[i-1] == word2[j-1]){
-            return dp[i][j] = recursion(i-1, j-1, word1, word2, dp);
-        }
-
-        int in = 1 + recursion(i, j-1, word1, word2, dp);
-        int del = 1 + recursion(i-1, j, word1, word2, dp);
-        int rep = 1 + recursion(i-1, j-1, word1, word2, dp);
-
-        return dp[i][j] = min(in, min(del, rep));
-    }
 public:
     int minDistance(string word1, string word2) {
         int n = word1.size();
         int m = word2.size();
-        vector<vector<int>> dp(n+1, vector<int> (m+1, -1));
-        return recursion(n, m, word1, word2, dp);
+        
+        vector<int> prev(m+1, 0);
+        for(int i=1;i<=m;i++){
+            prev[i] = i;
+        }
+        for(int i=1;i<=n;i++){
+            vector<int> curr(m+1, 0);
+            curr[0] = i;
+            for(int j=1;j<=m;j++){
+                if(word1[i-1] == word2[j-1]){
+                    curr[j] = prev[j-1];
+                }
+                else{
+                    int in = 1 + curr[j-1];
+                    int del = 1 + prev[j];
+                    int rep = 1 + prev[j-1];
+
+                    curr[j] = min(in, min(del, rep));
+                }
+            }
+            prev = curr;
+        }
+        return prev[m];;
     }
 };
