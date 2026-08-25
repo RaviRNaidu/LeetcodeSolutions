b687a14f1fe98c9b13006270ad92f5f9db1125fc
Ravi R Naidu <164465669+RaviRNaidu@users.noreply.github.com>
2026-08-26 00:27:14 +0530

    Time: 8 ms (36.47%) | Memory: 22.6 MB (38.56%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,27 +1,26 @@
 class Solution {
-    int recursion(int i, int take, vector<int>& prices, int n, vector<vector<int>> &dp){
-        if(i == n-1){
-            if(!take) return dp[i][take] = prices[n-1];
-            else return dp[i][take] = 0;
-        }
-
-        if(dp[i][take] != -1) return dp[i][take];
-
-        int notBuy = recursion(i+1, take, prices, n, dp);
-        int buy = 0;
-        if(take){
-            buy = - prices[i] + recursion(i+1, !take, prices, n, dp);
-        }
-        else if(!take){
-            buy = prices[i] + recursion(i+1, !take, prices, n, dp);
-        }
-
-        return dp[i][take] = max(notBuy, buy);
-    }
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n, vector<int> (2, -1));
-        return recursion(0, true, prices, n, dp);
+        vector<int> front(2, 0);
+        front[0] = prices[n-1];
+
+        for(int i=n-2;i>=0;i--){
+            vector<int> curr(2, 0);
+            for(int take=0;take<2;take++){
+                int notBuy = front[take];
+                int buy = 0;
+                if(take){
+                    buy = - prices[i] + front[!take];
+                }
+                else if(!take){
+                    buy = prices[i] + front[!take];
+                }
+
+                curr[take] = max(notBuy, buy);
+            }
+            front = curr;
+        }
+        return front[1];
     }
 };
