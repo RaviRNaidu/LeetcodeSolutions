ddfc2e5a84558681f859593a0bdd74a80ba82842
Ravi R Naidu <164465669+RaviRNaidu@users.noreply.github.com>
2026-08-27 13:51:37 +0530

    Time: 0 ms (100.00%) | Memory: 79.2 MB (88.81%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -5,9 +5,7 @@ public:
         vector<int> front(5, 0);
         front[1] = prices[n-1];
         front[3] = prices[n-1];
-
         for(int i=n-2;i>=0;i--){
-            vector<int> curr(5, 0);
             for(int cnt=3;cnt>=0;cnt--){
                 int notTake = front[cnt];
                 int take = 0;
@@ -17,9 +15,8 @@ public:
                 else{
                     take = prices[i] + front[cnt + 1];
                 }
-                curr[cnt] = max(notTake, take);
+                front[cnt] = max(notTake, take);
             }
-            front = curr;
         }
         return front[0];
     }
