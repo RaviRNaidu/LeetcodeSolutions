032778b30aa38c10c4f0e151050ccf930a57d727
Ravi R Naidu <164465669+RaviRNaidu@users.noreply.github.com>
2026-09-23 22:19:46 +0530

    Time: 8 ms (36.51%) | Memory: 102.4 MB (38.48%) - LeetSync

--- 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.cpp
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.cpp
@@ -2,30 +2,25 @@ class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
         int n = nums.size();
-        int ans = INT_MAX;
-        unordered_map<int, vector<int>> mpp;
         int sum = 0;
-        mpp[0].push_back(0);
         for(int i=0;i<n;i++){
             sum += nums[i];
-            mpp[sum].push_back(i+1);
-            if(sum == x) ans = min(ans, i+1);
         }
-        if(sum < x) return -1;
-
-        int sum2 = 0;
-        for(int i=n-1;i>=0;i--){
-            mpp.erase(sum);
-            sum -= nums[n-1];
-            sum2 += nums[i];
-            if(sum2 > x) break;
-            if(mpp.find(x-sum2) != mpp.end()){
-                for(auto it : mpp[x-sum2]){
-                    ans = min(ans, n-i+it);
-                }
+        
+        int ans = n+1;
+        int subSum = 0;
+        int l = 0;
+        int r = 0;
+        while(r <= n){
+            while(subSum > (sum - x) && l < r){
+                subSum -= nums[l];
+                l++;
             }
+            if(subSum == (sum - x)) ans = min(ans, n - (r-l)); 
+            if(r < n)subSum += nums[r];
+            r++;
         }
-        if(ans == INT_MAX) return -1;
+        if(ans == n+1) return -1;
         return ans;
     }
 };
